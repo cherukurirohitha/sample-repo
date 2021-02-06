74a3ebd893003f889ffa2cb107bf36ab1527ca7d
cherukurirohitha <40024833+cherukurirohitha@users.noreply.github.com>
2021-02-06 15:40:11 +0530

    Create hello-world.groovy

---
+++ hello-world.groovy
@@ -0,0 +1,28 @@
+@Library('TestLibrary')_
+pipeline {
+agent any
+stages{
+	stage('checkout'){
+		steps{
+		    
+			script{
+			    deleteDir()
+			    emailfile.sendemail()
+				checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'Github', url: 'https://github.com/cherukurirohitha/maven-hello-world']]])
+			}
+		}
+	}
+	
+	stage('build'){
+		steps{
+		    script{
+		     def mvnhome = tool 'M3'
+           
+              sh "cd my-app && '${mvnhome}/bin/mvn' clean install"
+            }  
+		    
+			
+		    }
+		}
+	}
+}
